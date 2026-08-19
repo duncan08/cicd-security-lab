7e5cdf7c37e729927044988ff5a2d59da7500bbc
Robinson Grullon <duncan.rose08@gmail.com>
2026-08-19 10:59:24 -0400

    TEST: intentionally break Terraform output reference to prove deployment stops on a broken plan

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,7 +5,7 @@ output "lambda_function_name" {
 
 output "lambda_function_arn" {
   description = "ARN of the deployed Lambda function"
-  value       = aws_lambda_function.app.arn
+  value       = aws_lambda_function.app.nonexistent_attribute
 }
 
 output "log_group_name" {
